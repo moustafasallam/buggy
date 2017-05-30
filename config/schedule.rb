e700fcb469d2fa7aa84481fd994520df66819ade
Moustafa Sallam <moustafasallam@hotmail.com>
2017-05-30 23:04:47 +0200

    final tweaks

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,7 @@ env :PATH, ENV['PATH']
 set :output, "#{path}/log/cron.log"
 set :bundle_command, "/usr/local/bin/bundle exec"
 set :environment, ENV['RAILS_ENV']
+ENV.each { |k, v| env(k, v) }
 
 every 1.minute do
   rake "bugs:create"
